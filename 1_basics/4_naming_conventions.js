1b547444284b3c0f7ef75c56486602bf811c42b1
Taexee <hee66265@naver.com>
2023-11-26 20:58:55 +0900

    변수명 짓는 룰

---
+++ 1_basics/4_naming_conventions.js
@@ -0,0 +1,28 @@
+/**
+ * Naming Conventions
+ * 
+ * 1) 일반적으로 영어를 사용하며 문자와 숫자를 모두 사용할 수 있다.
+ * 2) 특수기호는 _ $ 사용할 수 있다.
+ * 3) 숫자로 이름을 시작할 수 없다.
+ * 4) 키워드는 변수명으로 사용할 수 없다.
+ */
+
+let minghee = 'minghee';
+var $ming = 'ming';
+const _hee = 'hee';
+
+console.log(minghee, $ming, _hee);
+
+// let 1name = 'name';
+// let var = 'var;
+
+/**
+ * Naming Conventions2
+ * 
+ * 1) camelCase - 대부분의 언어에서 많이 사용
+ * 2) snake_case - 파이썬같은 언어에서 사용
+ * 3) PascalCasse - C# 마이크로소프트 계열의 언어에서 사용
+ */
+
+const mingHee = 'taexee';
+console.log(mingHee);
